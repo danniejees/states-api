3a94f7ded820f3554ba6515efa0538578f9fa5b0
Glitch (dj13-states-api) <none>
2025-05-01 03:35:49 +0000

    🐍🥕 Updated with Glitch

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const States = require('./States'); 
 const statesData = require('./statesData.json');
+const path = require('path'); 
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname)));  
 
 mongoose.connect(process.env.MONGODB_URI, { 
     useNewUrlParser: true, 
@@ -30,7 +33,7 @@ const validateState = (req, res, next) => {
 };
 
 app.get('/', (req, res) => {
-    res.send('<h1>Welcome to the US States API</h1>');
+    res.sendFile(path.join(__dirname, 'index.html'));  
 });
 
 app.get('/states/', async (req, res) => {
