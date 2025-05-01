3bd821767b401eb79ef2558028aa23ae168c0eb0
Glitch (cuddly-pitch-water) <none>
2025-05-01 02:50:17 +0000

    🖇️🍌 Updated with Glitch

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.log('Failed to connect to MongoDB', err));
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log("MongoDB connection error:", err));
 
 
 app.get('/states/', async (req, res) => {
